be776e4207fb1173453956475207b180eae83d13
lfnr <83223802+lfnr@users.noreply.github.com>
2021-06-09 08:17:06 -0300

    Create Classes.py

---
+++ Classes.py
@@ -0,0 +1,32 @@
+
+class Canhao(pygame.sprite.Sprite):
+    def __init__(self, groups, assets):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = assets['canhao_png']
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.left = WIDTH / 2
+        self.rect.centery = HEIGHT - 10 #A ajustar
+        self.groups = groups
+        self.assets = assets
+
+        # Só será possível atirar uma vez a cada 2000 milissegundos
+        self.last_shot = pygame.time.get_ticks()
+        self.shoot_ticks = 2000
+
+    def shoot(self):
+        # Verifica se pode atirar
+        now = pygame.time.get_ticks()
+        # Verifica quantos ticks se passaram desde o último tiro.
+        elapsed_ticks = now - self.last_shot
+
+        # Se já pode atirar novamente...
+        if elapsed_ticks > self.shoot_ticks:
+            # Marca o tick da nova imagem.
+            self.last_shot = now
+            # A nova bala vai ser criada logo acima e no centro horizontal da nave
+            new_bullet = Tiro(self.assets, self.rect.left, self.rect.centery)
+            self.groups[''].add(new_bullet)
+            self.groups[''].add(new_bullet)
+            self.assets[''].play()
