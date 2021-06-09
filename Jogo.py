cf84cd49b826c9507e8f2f0d18c06cecf8904821
lfnr <83223802+lfnr@users.noreply.github.com>
2021-06-09 19:27:56 -0300

    Update Jogo.py

--- Jogo.py
+++ Jogo.py
@@ -12,25 +12,6 @@ class Canhao(pygame.sprite.Sprite):
         # Só será possível atirar uma vez a cada 2000 milissegundos
         self.last_shot = pygame.time.get_ticks()
         self.shoot_ticks = 500
-
-
-
-    def shoot(self):
-        # Verifica se pode atirar
-        now = pygame.time.get_ticks()
-        # Verifica quantos ticks se passaram desde o último tiro.
-        elapsed_ticks = now - self.last_shot
-
-        # Se já pode atirar novamente...
-        if elapsed_ticks > self.shoot_ticks:
-            # Marca o tick da nova imagem.
-            self.last_shot = now
-            velx, vely = calculatePower(60)
-            # A nova bala vai ser criada logo acima e no centro horizontal do canhão
-            novo_tiro = Tiro(self.assets, velx, -vely)
-            self.groups['all_sprites'].add(novo_tiro)
-            self.groups['all_tiros'].add(novo_tiro)
-
               
             
         
